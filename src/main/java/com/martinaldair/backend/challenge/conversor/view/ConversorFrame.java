def7bb5b13039dd648f83adec96ccd0b951e47c9
Martin Aldair <martin.aldair.ramirez.ramos@outlook.com>
2023-10-06 14:54:02 -0600

    Ajuste de logica para los cuadros de diálogos

--- src/main/java/com/martinaldair/backend/challenge/conversor/view/ConversorFrame.java
+++ src/main/java/com/martinaldair/backend/challenge/conversor/view/ConversorFrame.java
@@ -32,11 +32,6 @@ public class ConversorFrame extends javax.swing.JFrame {
         });
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Conversor Divisas", "Conversor Temperatura" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
-            }
-        });
 
         jLabel1.setText("Seleccione una opcion de conversion");
 
@@ -73,93 +68,114 @@ public class ConversorFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    // Declaracion de variables globales
     Double number = 0.0;
     static String nuevaDivisaConvertida;
+    String inputDivisa = new String();
+    String divisaSeleccionada = new String();
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
         JComboBox inputTipoConvercion = jComboBox1;
         Integer indexConvertidorElegido = inputTipoConvercion.getSelectedIndex();
         // Tipo de conversion elegido por el usuario
-        switch (indexConvertidorElegido){
+        switch (indexConvertidorElegido) {
             case 0:
-            String moneda = JOptionPane.showInputDialog(this, "Ingrese la cantidad de dinero que deseas convertir:");
-            Boolean isValidoInput = ConversorUtil.validarInput(moneda);
-            // Valida el tipo de dato numerico de entrada
-            if(!isValidoInput){
-                  JOptionPane.showMessageDialog(this, "Valor de entrada no valido", "Error", JOptionPane.ERROR_MESSAGE);
-            }else {
-            String [] inputValoresMenu = { "De pesos MX a Dolar", "De pesos MX a Euros", "De pesos MX a Libras Esterlinas", 
-                "De pesos MX a Yen Japonés", "De pesos MX a Won sul-coreano", "De Dolar a pesos MX", "De Euros a pesos MX", 
-                "De Libras Esterlinas a pesos MX", "De Yen Japonés a pesos MX", "De Won sul-coreano a pesos MX" };
-
-            String  monedaSeleccionada = (String) JOptionPane.showInputDialog(null,
-             "Elige la moneda a la que deseas convertir tu dinero", "Monedas",
-             JOptionPane.INFORMATION_MESSAGE, null,
-             inputValoresMenu, null);
-            
-            number = Double.parseDouble(moneda);
-            
-            switch (monedaSeleccionada) {
-                case "De pesos MX a Dolar":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "usa", number);
-                    break;
-                case "De pesos MX a Euros":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "eur", number);
-                    break;
-                case "De pesos MX a Libras Esterlinas":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "gbp", number);
-                    break;
-                case "De pesos MX a Yen Japonés":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "yen", number);
-                    break;
-                case "De pesos MX a Won sul-coreano":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "won", number);
-                    break;
-                case "De Dolar a pesos MX":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("usa", "mx", number);
-                    break;
-                case "De Euros a pesos MX":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("eur", "mx", number);
-                    break;
-                case "De Libras Esterlinas a pesos MX":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("gbp", "mx", number);
-                    break;
-                case "De Yen Japonés a pesos MX":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("yen", "mx", number);
-                    break;
-                case "De Won sul-coreano a pesos MX":
-                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("won", "usa", number);
-                    break;
+                // Se declara el cuadro de dialogo - Menu principal
+                inputDivisa = (String) JOptionPane.showInputDialog(this, "Ingrese la cantidad de dinero que deseas convertir:");
+                // El usuario digito algo y dio click al boton OK
+                if(inputDivisa != null){
+                   // El usuario coloco algo que no sea solo espacios
+                   if(!inputDivisa.trim().equals("")){
+                       // Valida el tipo de dato que sea numerico
+                       Boolean isValidoInputDivisa = ConversorUtil.validarInput(inputDivisa);
+                       if(!isValidoInputDivisa){
+                            // Se declara el cuadro de dialogo - Manejo de errores
+                            JOptionPane.showMessageDialog(this, "Valor de entrada no valido", "Error", JOptionPane.ERROR_MESSAGE);
+                       }else{
+                            // Declaracion de valores del menu de divisas
+                            String [] inputValoresMenu = { "De pesos MX a Dolar", "De pesos MX a Euros", "De pesos MX a Libras Esterlinas", 
+                            "De pesos MX a Yen Japonés", "De pesos MX a Won sul-coreano", "De Dolar a pesos MX", "De Euros a pesos MX", 
+                            "De Libras Esterlinas a pesos MX", "De Yen Japonés a pesos MX", "De Won sul-coreano a pesos MX" };
+                            // Se declara el cuadro de dialogo - Opciones de monedas
+                            divisaSeleccionada = (String) JOptionPane.showInputDialog(this, "Elige la moneda a la que deseas convertir tu dinero", "Monedas",
+                            JOptionPane.INFORMATION_MESSAGE, null,inputValoresMenu, null);
+                            if (divisaSeleccionada != null){
+                                number = Double.parseDouble(inputDivisa);
+                                switch (divisaSeleccionada) {
+                                case "De pesos MX a Dolar":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "usa", number);
+                                    break;
+                                case "De pesos MX a Euros":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "eur", number);
+                                    break;
+                                case "De pesos MX a Libras Esterlinas":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "gbp", number);
+                                    break;
+                                case "De pesos MX a Yen Japonés":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "yen", number);
+                                    break;
+                                case "De pesos MX a Won sul-coreano":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("mx", "won", number);
+                                    break;
+                                case "De Dolar a pesos MX":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("usa", "mx", number);
+                                    break;
+                                case "De Euros a pesos MX":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("eur", "mx", number);
+                                    break;
+                                case "De Libras Esterlinas a pesos MX":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("gbp", "mx", number);
+                                    break;
+                                case "De Yen Japonés a pesos MX":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("yen", "mx", number);
+                                    break;
+                                case "De Won sul-coreano a pesos MX":
+                                    nuevaDivisaConvertida = ConversorUtil.conversorDivisas("won", "usa", number);
+                                    break;
+                                }
+                                // Declaracion de valores del menu
+                                Object[] optionsMsgValorConversion = { "Aceptar" };
+                                Object[] optionsMsgContinuarPrograma = { "Si", "No", "Cancelar" };
+                                // Se declara el cuadro de dialogo - Valor de la conversion
+                                Integer respValorConversion = JOptionPane.showOptionDialog(null, "Tienes "+ nuevaDivisaConvertida, "Mensaje",
+                                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, optionsMsgValorConversion, optionsMsgValorConversion[0]);
+                                if(respValorConversion == 0){
+                                    // Se declara el cuadro de dialogo ¿Desea continuar?
+                                    int respContinuarPrograma = JOptionPane.showOptionDialog(null, "Deseas continuar", "Selecciona una opcion",
+                                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                                            null, optionsMsgContinuarPrograma, optionsMsgContinuarPrograma[0]);
+                                    if(respContinuarPrograma!= 0){
+                                        // Se declara el cuadro de dialogo Finalizar programa
+                                        JOptionPane.showMessageDialog(this, "Programa Finalizado", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
+                                        System.exit(0);
+                                    }else{
+                                        number = 0.0;
+                                        nuevaDivisaConvertida = "";
+                                    }
+                                }
+                            }else{
+                                // El usuario dio click al boton Cancelar
+                                // Se declara el cuadro de dialogo - Cerror opcion
+                                JOptionPane.showMessageDialog(this, "Cerrar opcion de conversion", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
+                            }
+                       }
+                   }else{
+                      // El usuario coloco solo espacios en blanco o tabulaciones
+                      // Se declara el cuadro de dialogo - Menejo de errores
+                      JOptionPane.showMessageDialog(this, "Valor requerido", "ERROR", JOptionPane.ERROR_MESSAGE);
+                   }
+                }else{
+                   // El usuario dio click al boton Cancel
+                   // Se declara el cuadro de dialogo - Cerrar opcion
+                   JOptionPane.showMessageDialog(this, "Cerrar opcion de conversion", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
                 }
-            }
-            Object[] optionsMsgValorConversion = { "Aceptar" };
-            Object[] optionsMsgContinuarPrograma = { "Si", "No", "Cancelar" };
-            int respValorConversion = JOptionPane.showOptionDialog(null, "Tienes "+ nuevaDivisaConvertida, "Mensaje",
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
-                    null, optionsMsgValorConversion, optionsMsgValorConversion[0]);
-            if(respValorConversion ==0) {
-            	int respContinuarPrograma = JOptionPane.showOptionDialog(null, "Deseas continuar", "Selecciona una opcion",
-                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,
-                        null, optionsMsgContinuarPrograma, optionsMsgContinuarPrograma[0]);
-            	if(respContinuarPrograma!= 0) {
-            		JOptionPane.showMessageDialog(this, "Programa Finalizado", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
-            		System.exit(0);
-            	}else {
-            		number = 0.0;
-            		nuevaDivisaConvertida = "";
-            	}
-            }
             break;
             case 1:
             break;
-    }//GEN-LAST:event_jButton1ActionPerformed
+        }//GEN-LAST:event_jButton1ActionPerformed
     }
     
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<String> jComboBox1;
