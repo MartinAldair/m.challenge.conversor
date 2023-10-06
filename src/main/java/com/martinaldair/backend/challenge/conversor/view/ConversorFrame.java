58ee3b5953dad7c9b7795dd56a3b4997e72a65a0
Martin Aldair <martin.aldair.ramirez.ramos@outlook.com>
2023-10-05 23:15:36 -0700

    Se agrega logica para los cuadros de diálogos - Menú Principal, Entrada de valor, Opciones de Monedas y Manejo de errores

--- src/main/java/com/martinaldair/backend/challenge/conversor/view/ConversorFrame.java
+++ src/main/java/com/martinaldair/backend/challenge/conversor/view/ConversorFrame.java
@@ -1,22 +1,158 @@
 package com.martinaldair.backend.challenge.conversor.view;
 
+import javax.swing.*;
+import javax.swing.JOptionPane;
+import java.util.regex.*;
+
 /**
  *
- * @author Martin_Aldair
+ * @author Martin Aldair
  */
 public class ConversorFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form ConversorFrame
-     */
     public ConversorFrame() {
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-    	
+
+        jButton1 = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Aceptar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Conversor Divisas", "Conversor Temperatura" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Seleccione una opcion de conversion");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(89, 89, 89)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(116, 116, 116)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(43, 43, 43)
+                                .addComponent(jButton1)))))
+                .addContainerGap(101, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(105, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(31, 31, 31)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addComponent(jButton1)
+                .addGap(74, 74, 74))
+        );
+
         pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    Double number = 0.0;
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        JComboBox inputTipoConvercion = jComboBox1;
+        Integer indexConvertidorElegido = inputTipoConvercion.getSelectedIndex();
+        // Tipo de conversion elegido por el usuario
+        switch (indexConvertidorElegido){
+            case 0:
+            String moneda = JOptionPane.showInputDialog(this, "Ingrese la cantidad de dinero que deseas convertir:");
+            Boolean isValidoInput = validarInput(moneda);
+            // Valida el tipo de dato numerico de entrada
+            if(!isValidoInput){
+                  JOptionPane.showMessageDialog(this, "Valor de entrada no valido", "Error", JOptionPane.ERROR_MESSAGE);
+            }else{
+            Object[] inputValoresMenu = { "De pesos MX a Dolar", "De pesos MX a Euros", "De pesos MX a Libras Esterlinas", 
+                "De pesos MX a Yen Japonés", "De pesos MX a Won sul-coreano", "De Dolar a pesos MX", "De Euros a pesos MX", 
+                "De Libras Esterlinas a pesos MX", "De Yen Japonés a pesos MX", "De Won sul-coreano a pesos MX" };
+
+            Object monedaSeleccionada = JOptionPane.showInputDialog(null,
+             "Elige la moneda a la que deseas convertir tu dinero", "Monedas",
+             JOptionPane.INFORMATION_MESSAGE, null,
+             inputValoresMenu, null);
+            }
+            // number = Double.parseDouble(moneda); 
+            break;
+            case 1:
+            break;
+    }//GEN-LAST:event_jButton1ActionPerformed
+    }
+    
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ConversorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ConversorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ConversorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ConversorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ConversorFrame().setVisible(true);
+            }
+        });
     }
 
+    /*
+     *Metodo de validadacion de tipo numerico
+     **/
+    Boolean validarInput(String inputMoneda){
+        Boolean isValido = Pattern.compile("[0-9.]+").matcher(inputMoneda).matches();
+        return isValido;
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
 }
